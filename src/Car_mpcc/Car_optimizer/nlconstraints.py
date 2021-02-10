981769b7e3d5bf404d5d7fde13c0ae7fb4a440b2
EnriMion <36770665+EnriMion@users.noreply.github.com>
2021-02-10 18:02:44 +0100

    Little changes

--- src/Car_mpcc/Car_optimizer/nlconstraints.py
+++ src/Car_mpcc/Car_optimizer/nlconstraints.py
@@ -71,36 +71,3 @@ def nlconst_carN(z, p):
 
     v = np.array([v1, v2, v3])
     return v
-
-def nlconst_car_pg(z, p):
-    """
-    Computes the nonlinear inequality constraints.
-
-    :param z: States and inputs of the system dynamics
-    :param p: Parameters of the system dynamics
-    :return: List containing the constraints #todo
-    """
-    slack = z[params.i_idx.slack]
-
-    pointsO = params.n_param
-    pointsN = params.n_bspline_points
-    points = getPointsFromParameters(p, pointsO, pointsN)
-    radii = getRadiiFromParameters(p, pointsO, pointsN)
-
-    splx, sply = casadiDynamicBSPLINE(z[params.s_idx.s], points)
-    splsx, splsy = casadiDynamicBSPLINEsidewards(z[params.s_idx.s], points)
-    r = casadiDynamicBSPLINERadius(z[params.s_idx.s], radii)
-
-    wantedpos = vertcat(splx, sply)
-    sidewards = vertcat(splsx, splsy)
-
-    realPos = np.array([[z[params.s_idx.x]], [z[params.s_idx.y]]])
-    centerPos = realPos
-    error = centerPos - wantedpos
-    laterror = mtimes(sidewards.T, error)
-
-    v1 = laterror - r - slack
-    v2 = -laterror - r - slack
-
-    v = np.array([v1, v2])
-    return v
