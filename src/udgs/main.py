a6959ce346cdd06fc5fad7be0189c2b029c3308c
alezana <azanardi@ethz.ch>
2021-04-20 12:39:41 +0200

    misc

--- src/udgs/main.py
+++ src/udgs/main.py
@@ -1,7 +1,9 @@
 import argparse
+import os.path
 
 from udgs.forces_models.model_def.sim import sim_car_model
-from udgs.forces_models.model_def.generate_model import AVAILABLE_METHODS, generate_forces_models, SolutionMethod, LexicographicPG
+from udgs.forces_models.model_def.generate_model import AVAILABLE_METHODS, generate_forces_models, SolutionMethod, \
+    LexicographicPG
 from udgs.forces_models.model_def.sim_report import make_report
 
 
@@ -26,8 +28,14 @@ def _parse_args():
         type=int,
     )
     p.add_argument(
+        "--output_dir",
+        default="out-docker",
+        help="todo",
+        type=str,
+    )
+    p.add_argument(
         "--solution_method",
-        default="PG",
+        default="LexicographicPG",
         help="PG: potential game solution,"
              "LexicographicPG: Lexicographic potential game"
              "IBR: Iterated best response"
@@ -41,7 +49,8 @@ def _parse_args():
 def main(generate_solver: bool = True,
          to_deploy: bool = False,
          n_players: int = 3,
-         solution_method: SolutionMethod = LexicographicPG):
+         solution_method: SolutionMethod = LexicographicPG,
+         output_dir: str = "out-docker"):
     if solution_method not in AVAILABLE_METHODS:
         raise ValueError(f"{solution_method} is not available. Retry with one amongst {AVAILABLE_METHODS}")
     # generate forces models definition and solvers
@@ -52,7 +61,9 @@ def main(generate_solver: bool = True,
     sim_data = sim_car_model(model, solver, n_players, solution_method, sim_length=5)
     # visualisation and report of data
     report = make_report(sim_data, solution_method)
-    report.to_html("udgs_report.html")
+    # save report
+    report_file = os.path.join(output_dir, "udgs_report.html")
+    report.to_html(report_file)
 
 
 if __name__ == "__main__":
